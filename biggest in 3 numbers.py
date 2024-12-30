9756d6f55636152218e7d3e2a1c17cf9cca0292c
Arathi-vv <arathivv8086@gmail.com>
2024-12-30 14:08:23 +0530

    Add files via upload

---
+++ biggest in 3 numbers.py
@@ -0,0 +1,10 @@
+print("Enter 3 numbers:")
+n1=int(input())
+n2=int(input())
+n3=int(input())
+if n1>n2 and n1>n3:
+    print(n1,"is largest")
+elif n2>n3 and n2>n3:
+    print(n2, "is largest")
+else:
+    print(n3,"is largest")
