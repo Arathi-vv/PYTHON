8aa36a9ed53bd7b1a27a0529fe01f6b7e2f067db
Arathi-vv <arathivv8086@gmail.com>
2024-12-30 13:36:48 +0530

    Add files via upload

---
+++ Leap year.py
@@ -0,0 +1,6 @@
+n=int(input("Enter the current year:"))
+f=int(input("Enter the final year:"))
+print("The future leap years are:")
+for x in range(n,f+1):
+   if(x%4==0 and x%100!=0)or(x%400==0):
+         print(x)
